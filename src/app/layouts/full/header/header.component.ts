e2062c88f3a94655daf4d791fe1816de96faa4c6
renatu <renatu_mb@yahoo.com.br>
2024-01-14 17:07:19 -0800

    14 - Log out functionality ( Confirmation.component )

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {ConfirmationComponent} from '../../../material-component/dialog/confirmation/confirmation.component';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +10,27 @@ import { Component } from '@angular/core';
 })
 export class AppHeaderComponent {
 
-  constructor() {
+  constructor(private router: Router,
+              private matDialog: MatDialog) {
+  }
+
+  logout(): void {
+    const matDialogConfig = new MatDialogConfig();
+    matDialogConfig.data = {
+      messageX: 'Logout',
+      confirmation: true
+    };
+
+    const popup = this.matDialog.open(ConfirmationComponent, matDialogConfig);
+
+    popup.componentInstance.onEmitStatusChange.subscribe(
+      {
+        next: (): void => {
+          popup.close();
+          localStorage.clear();
+          this.router.navigate(['/']);
+        }
+      }
+    );
   }
 }
